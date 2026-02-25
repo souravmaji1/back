1be3008b1cdb103a9822f266670eb8312c14084e
souravmaji1 <majisourav255@gmail.com>
2026-02-25 22:01:59 +0530

    u

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 import express from 'express';
 import crypto from 'crypto';
+import cors from 'cors';
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const dockerComposeRaw = `services:
   openclaw:
